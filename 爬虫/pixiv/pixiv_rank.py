44aabe25c0dcb27779f10a09cf145a1034a872c1
c1529 <104741605+c1529@users.noreply.github.com>
2023-09-02 14:48:07 +0800

    Update pixiv_rank.py

--- 爬虫/pixiv/pixiv_rank.py
+++ 爬虫/pixiv/pixiv_rank.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 import test1
 
-with ThreadPoolExecutor(max_workers=4) as k:
+with ThreadPoolExecutor(max_workers=32) as k:
     for page in range(1, 100):
         try:
             k.submit(test1.main, page)
